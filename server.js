6396c33943fe2e10333994d1dcd43c984ba7d168
Homie-2205 <cmacmacma2205@hotmail.com>
2026-09-01 22:36:28 -0500

    Change module import from someModule to index

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Node + FFmpeg server is running');
+});
+
+// Import and use other files
+const someModule = require('./index');
+
+// Example endpoint using another file
+app.get('/process', (req, res) => {
+  const result = someModule.doSomething();
+  res.json({ result });
+});
+
+// Start server on Render port
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});

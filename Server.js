ed4bdad2cbbc2b3be7331b0ad737960ac32b034c
Homie-2205 <cmacmacma2205@hotmail.com>
2026-09-06 21:53:00 -0500

    Set up Express server with basic route

--- Server.js
+++ Server.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
 
+// Render automatically assigns a PORT variable
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Server running successfully on Render!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
